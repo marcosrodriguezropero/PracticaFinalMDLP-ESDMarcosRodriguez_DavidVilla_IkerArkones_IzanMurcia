867bf79f02838f3a5aceebf382a01194b56b84db
davidvillap <david.villap@edu.uah.es>
2025-06-10 12:12:17 +0200

    Update IABasica.java

--- PracticaFinalMDLP-ESDMarcosRodriguez_DavidVilla_IkerArkones_IzanMurcia-main (1)/PracticaFinalMDLP-ESDMarcosRodriguez_DavidVilla_IkerArkones_IzanMurcia-main/src/practicafinal/clases/IABasica.java
+++ PracticaFinalMDLP-ESDMarcosRodriguez_DavidVilla_IkerArkones_IzanMurcia-main (1)/PracticaFinalMDLP-ESDMarcosRodriguez_DavidVilla_IkerArkones_IzanMurcia-main/src/practicafinal/clases/IABasica.java
@@ -18,10 +18,10 @@ public class IABasica {
             Unidad unidad = unidadesIA.obtener(i);
             if (!unidad.estaViva()) continue;
 
-            Unidad obj = UnidadEnemigaMasCercana(unidad);
+            Unidad obj = unidadEnemigaMasCercana(unidad);
             if (obj == null) continue;
 
-            if (RangoAtaque(unidad, obj)) {
+            if (rangoAtaque(unidad, obj)) {
                 atacar(unidad, obj);
             } else {
                 mover(unidad, obj);
@@ -29,14 +29,14 @@ public class IABasica {
         }
     }
 
-    private Unidad UnidadEnemigaMasCercana(Unidad unidad) {
+    private Unidad unidadEnemigaMasCercana(Unidad unidad) {
         Unidad cercana = null;
         int distanciaMinima = Integer.MAX_VALUE;
 
         ListaEnlazada<Unidad> unidadesJugador = jugador.getUnidades();
         for (int i = 0; i < unidadesJugador.tamanio(); i++) {
-            Unidad unidad1 = unidad;
-            if (unidad1 == null || unidad.estaViva()) continue;
+            Unidad unidad1 = unidadesJugador.obtener(i);
+            if (unidad1 == null || !unidad1.estaViva()) continue;
 
             int distancia = Math.abs(unidad1.getFila() - unidad.getFila()) + Math.abs(unidad1.getColumna() - unidad.getColumna());
             if (distancia < distanciaMinima) {
@@ -47,21 +47,21 @@ public class IABasica {
         return cercana;
     }
 
-    private boolean RangoAtaque (Unidad atacante, Unidad obj){
+    private boolean rangoAtaque(Unidad atacante, Unidad obj) {
         int distancia = Math.abs(atacante.getColumna() - obj.getColumna()) + Math.abs(atacante.getFila() - obj.getFila());
-        return distancia <= atacante.rangoAtaque;
+        return distancia <= atacante.getRangoAtaque();
     }
 
     private void atacar(Unidad atacante, Unidad defensor) {
         int factor = (int)(Math.random() * 3);
-        int danio = Math.max(0, factor * atacante.ataque - defensor.defensa);
+        int danio = Math.max(0, factor * atacante.getAtaque() - defensor.getDefensa());
 
         defensor.hp -= danio;
 
-        System.out.println(atacante.getNombre() + "ataca a " + defensor.getNombre() + "causando" + danio + "de daño");
+        System.out.println(atacante.getNombre() + " ataca a " + defensor.getNombre() + " causando " + danio + " de daño");
 
         if (defensor.hp <= 0) {
-            System.out.println(defensor.getNombre() + "ha sido eliminado");
+            System.out.println(defensor.getNombre() + " ha sido eliminado");
             tablero.getCasilla(defensor.getFila(), defensor.getColumna()).quitarUnidad();
             jugador.getUnidades().eliminar(defensor);
         }
@@ -71,8 +71,8 @@ public class IABasica {
         int filaPrim = unidad.getFila();
         int columnaPrim = unidad.getColumna();
 
-        int filaFin = unidad.getFila();
-        int columnaFin = unidad.getColumna();
+        int filaFin = filaPrim;
+        int columnaFin = columnaPrim;
 
         if (obj.getFila() > filaPrim) filaFin++;
         else if (obj.getFila() < filaPrim) filaFin--;
@@ -86,8 +86,10 @@ public class IABasica {
             tablero.getCasilla(filaPrim, columnaPrim).quitarUnidad();
             destino.colocarUnidad(unidad);
 
-            System.out.println(unidad.getNombre() + "se mueve a (" + filaFin + "," + columnaFin + ")");
+            unidad.setFila(filaFin);
+            unidad.setColumna(columnaFin);
+
+            System.out.println(unidad.getNombre() + " se mueve a (" + filaFin + "," + columnaFin + ")");
         }
     }
-
 }
