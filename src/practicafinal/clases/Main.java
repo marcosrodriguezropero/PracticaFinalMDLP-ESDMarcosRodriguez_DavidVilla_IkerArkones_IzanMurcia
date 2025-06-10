933b37f294e160efa3049eb54e23e40f29c1aa74
marcosrodriguezropero <marcosrodriguezropero@gmail.com>
2025-06-10 12:17:06 +0200

    Delete src/practicafinal directory

--- src/practicafinal/clases/Main.java
+++
@@ -1,9 +0,0 @@
-package practicafinal.clases;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(InterfazTablero.class, args);
-    }
-}
